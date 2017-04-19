b4a765bc7a5f6d924e9ec17e2e0bc05be0463ef8
xiaoqixuan <212538776@qq.com>
2017-04-19 21:33:19 +0800

    添加枚举基础api

---
+++ JavaSe-Enum/src/main/java/com/worldly/enums/EnumCreateByInterface.java
@@ -0,0 +1,16 @@
+package com.worldly.enums;
+
+/**
+ * 通过接口实现枚举
+ *
+ * @author Worldly
+ * @create 2017-04-18 17:29
+ **/
+public class EnumCreateByInterface{
+    
+}
+
+interface Season{
+    void say();
+    void doSomething();
+}
