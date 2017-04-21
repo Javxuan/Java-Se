d8a0f466b2b96cefd12ac2efae0d2c69e162118f
xiaoqixuan <212538776@qq.com>
2017-04-21 15:29:23 +0800

    DOM解析xml 与生成xml

---
+++ JavaSe-XML/src/main/java/com/worldly/xml/Dog.java
@@ -0,0 +1,36 @@
+package com.worldly.xml;
+
+/**
+ * 狗对象
+ *
+ * @author Worldly
+ * @create 2017-04-20 20:31
+ **/
+public class Dog {
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
